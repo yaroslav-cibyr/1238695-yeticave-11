c2b1c5e6d93632f80167b19eaccd43dae0f21079
yaroslav-cibyr <zolot.yaroslav@yandex.ru>
2019-11-03 15:39:20 +0200

    module3-task1 Повторно сделал задания

--- index.php
+++ index.php
@@ -1,25 +1,9 @@
 <?php
 
-function include_template($name, array $data = [])
-{
-    $name = 'templates/' . $name;
-    $result = '';
-
-    if (!is_readable($name)) {
-        return $result;
-    }
-
-    ob_start();
-    extract($data);
-    require $name;
-
-    $result = ob_get_clean();
-
-    return $result;
-}
+require_once "helpers.php";
 
 
-$pageContent = include_template('main.php',);
+$pageContent = include_template('main.php');
 $page = include_template('layout.php', [
     'main' => $pageContent,
     'title' => 'Главная страница',
