ab82592ccc7a769ef8ff8a4851cd16e3761d5be3
yaroslav-cibyr <56890857+yaroslav-cibyr@users.noreply.github.com>
2019-11-06 16:00:25 +0200

    Update main.php

--- templates/main.php
+++ templates/main.php
@@ -16,22 +16,22 @@
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <?php foreach ($lots as $product): ?>
+            <?php foreach ($lots as $lot): ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="<?php echo $product["img"] ?>" width="350" height="260" alt="">
+                        <img src="<?php echo $lot["img"] ?>" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?php echo $product["categories"] ?></span>
+                        <span class="lot__category"><?php echo $lot["categories"] ?></span>
                         <h3 class="lot__title"><a class="text-link"
-                                                  href="pages/lot.html"><?php echo $product["name"] ?></a></h3>
+                                                  href="pages/lot.html"><?php echo $lot["name"] ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?php echo formatPrice($product["price"]) ?></span>
+                                <span class="lot__cost"><?php echo formatPrice($lot["price"]) ?></span>
                             </div>
                             <?php
-                            $timeLeft = timeLeft($product["expirationDate"]);
+                            $timeLeft = timeLeft($lot["expirationDate"]);
                             $timerFinishing = $timeLeft[0] < 1 ? "timer--finishing" : "";
                             ?>
                             <div class="lot__timer timer <?php echo $timerFinishing; ?>">
