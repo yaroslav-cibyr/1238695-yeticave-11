81575b963b35cf44a9da1db8b27d7e2f72cf1d24
yaroslav-cibyr <zolot.yaroslav@yandex.ru>
2019-11-11 20:16:49 +0200

    queries.sql  -_-

--- queries.sql
+++ queries.sql
@@ -1,6 +1,8 @@
+USE yeticave
 INSERT INTO user (date_of_registration,email,name,password,contacts) values
 ('2019-11-12 22:36:00','ivanova@it.com','Ivanova','0507','Sample text'),
 ('2019-11-13 22:36:00','ivano@it.com','Ivanov','1234','Sample text');
+
 INSERT INTO category (title,code) values
 ('Доски и лыжи','boards'),
 ('Крепления','attachment'),
@@ -17,23 +19,30 @@ INSERT INTO category (title,code) values
 ('Ботинки для сноуборда DC Mutiny Charocal', '5', '1','2','10999','200','img/lot-4.jpg','2019-12-24','sample text','2019-11-11 22:44:00'),
 ('Куртка для сноуборда DC Mutiny Charocal', '6', '2','1','7500','100','img/lot-5.jpg','2019-12-24','sample text','2019-11-11 22:44:00'),
 ('"Маска Oakley Canopy', '7', '1','2','5400','250','img/lot-6.jpg','2019-12-24','sample text','2019-11-11 22:44:00');
+
 INSERT INTO bid (date,amount,user_id,lot_id) values
-('2019-11-11 23:10:00','11250','2','1');
+('2019-11-11 23:10:00','11140','2','1'),
+('2019-11-11 23:11:00','11145','2','1'),
+('2019-11-11 23:12:00','11150','2','1'),
+('2019-11-11 23:12:00','11245','2','1'),
+('2019-11-11 23:13:00','11250','2','1'),
+('2019-11-11 23:14:00','11340','2','1');
 
 
 -- получить все категории;
 SELECT * FROM category;
 
 -- получить самые новые, открытые лоты. Каждый лот должен включать название, стартовую цену, ссылку на изображение, цену, название категории;
-SELECT name, starting_price, image, category_id FROM lot i
-JOIN category c
+SELECT * FROM lot i
+JOIN category  c
 ON i.category_id = c.id
+WHERE expiration_date > now()
 ORDER BY creation_date ASC;
 
 -- показать лот по его id. Получите также название категории, к которой принадлежит лот;
 SELECT * FROM lot i
 JOIN category  c
-ON i.category_id = c.id
+ ON i.category_id = c.id
 WHERE i.id = 1;
 
 -- обновить название лота по его идентификатору;
@@ -41,10 +50,6 @@ UPDATE lot SET name = '2014 Rossignol District Snowboard 2'
 WHERE id = 1;
 
  -- получить список ставок для лота по его идентификатору с сортировкой по дате.
-SELECT amount FROM bid b
-JOIN lot i
-ON lot_id = i.id
-WHERE i.id = 1
-ORDER BY i.creation_date ASC;
+SELECT * FROM bid where lot_id = 1 ORDER BY DATE desc;
 
-CREATE UNIQUE INDEX emailkey ON user(email);
+
